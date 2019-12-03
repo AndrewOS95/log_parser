1d94d25efb1195bae95843af5de422724cd8e95d
AndrewOS95 <cata112233@gmail.com>
2019-12-03 21:26:19 +0000

    Separate logic into specific methods and add options parser

--- parser.rb
+++ parser.rb
@@ -1,29 +1,50 @@
+require "optparse"
 class Parser
-  def self.get_endpoint(file_path)
-    file = File.open(file_path, "r")
+  def self.read_and_validate(file_path)
     @returns = ""
+    @file = File.open(file_path, "r")
     @arr = []
-    @endpoints = []
     @counts = Hash.new(0)
     regex = /^(\/[a-z0-9_]+){1,} [0-9]{3}.[0-9]{3}.[0-9]{3}.[0-9]{3}/
-    file.each do |row|
+    @file.each do |row|
       if row =~ regex
         @arr << row
       end
     end
+    return @arr
+  end
+
+  def self.transform_data
+    @endpoints = []
+    @addresses = []
     @arr.each do |item|
         @endpoints << item.split(' ')[0]
-    #return @arr
+        @addresses << item.split(' ')[1]
     end
+    return @endpoints, @addresses
+  end
 
+  def self.read_endpoints
     @endpoints.each { |item| @counts[item] += 1 }
     item = @counts.sort_by {|key, value| value}.reverse
     item.each do |i|
        @returns += "There were #{i[1]} visits for '#{i[0]}' endpoint\n"
     end
-    return @returns
+    @returns
   end
+end
 
-puts get_endpoint("./webserver.log")
 
+opts = {}
+OptionParser.new do |opt|
+  opt.on('-f', '--file FILE') {|o| opts[:file] = o }
+  opt.on('-e', '--get_endpoints')
+end.parse!(into: opts)
+
+if opts[:file] && opts[:get_endpoints]
+  Parser.read_and_validate(opts[:file])
+  Parser.transform_data
+  puts Parser.read_endpoints
+else
+  puts "Please import data through a file"
 end
